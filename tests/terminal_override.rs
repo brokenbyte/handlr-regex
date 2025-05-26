3aa7f8e5e7839bc164dc08803dbee40691097093
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2025-05-26 16:23:40 -0400

    tests: update integration tests to test logs

--- tests/terminal_override.rs
+++ tests/terminal_override.rs
@@ -5,17 +5,34 @@ use std::process::Command;
 fn test_terminal_output(terminal_output: bool) -> Command {
     let mut cmd = Command::new(get_cargo_bin("handlr"));
     cmd.arg(format!("--force-terminal-output={}", terminal_output))
+        .arg("-vvv") // Maximum verbosity
+        .arg("--disable-notifications") // Not much point showing these in tests
         .arg("mime")
         .arg("./assets");
     cmd
 }
 
+/// Helper function for insta settings
+fn timestamp_filter() -> Vec<(&'static str, &'static str)> {
+    vec![(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d*\.\d*Z", "[TIMESTAMP]")]
+}
+
 #[test]
 fn terminal_output_tests_force_true() {
-    assert_cmd_snapshot!(test_terminal_output(true))
+    insta::with_settings!(
+        {
+            filters => timestamp_filter()
+        },
+        { assert_cmd_snapshot!(test_terminal_output(true)) }
+    )
 }
 
 #[test]
 fn terminal_output_tests_force_false() {
-    assert_cmd_snapshot!(test_terminal_output(false))
+    insta::with_settings!(
+        {
+            filters => timestamp_filter()
+        },
+        { assert_cmd_snapshot!(test_terminal_output(false)) }
+    )
 }
